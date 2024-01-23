3601d513e7f5adc2544a0566f8528f2c65e01cda
Leonardo de Melo <45073941+LeoBardineo@users.noreply.github.com>
2024-01-23 00:50:28 -0300

    style inline do grid agr ta bem melhor pq n pensei nisso antes

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -9,8 +9,10 @@ interface MateriaCard {
 const Card = ({ id, nome, hInicial, horas, dia }: MateriaCard) => {
   return (
     <div
-      className={`card bg-red-500 row-start-${hInicial - 5}
-      row-end-${hInicial - 5 + horas} col-start-${dia + 1}`}
+      className='card bg-red-500'
+      style={{gridRowStart: hInicial - 5,
+              gridRowEnd: hInicial - 5 + horas,
+              gridColumnStart: dia + 1}}
     >
       <h1>{id}</h1>
       {horas > 1 ? <h2>{nome}</h2> : ''}
